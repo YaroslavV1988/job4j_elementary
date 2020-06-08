2b0b41c9689bb1b0e8ac84e040cfe061d442a272
YaroslavV1988 / <yaroslav-voronkov@yandex.ru>
2020-06-08 15:53:25 +0300

    9.1. String[237407#244687]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+import java.sql.SQLOutput;
+
+public class Greeting {
+    public static void main(String[] args) {
+String idea = "I like Java!";
+        System.out.println(idea);
+        String think = "But I am a newbie.";
+        String id = idea + think;
+        System.out.println(id);
+      int year = 2020;
+      String ideaPlusId = idea + year;
+        System.out.println(ideaPlusId);
+    }
+}
